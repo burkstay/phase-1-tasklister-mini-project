28cfc88e270fd3af121820326643d24163a83b3f
Taylor Burks <tayyy25@gmail.com>
2023-05-08 05:05:09 -0400

    task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  addingEventListeners()
 });
+
+// get the form and add an event listener
+function addingEventListeners() {
+  document.getElementById("create-task-form").addEventListener("submit", handleFormSubmit)
+}
+
+function handleFormSubmit(e) {
+  e.preventDefault()
+  // console.log(e)
+  const task = e.target[0].value
+  displayTask(task)
+}
+
+function displayTask(task) {
+  const taskUl = document.getElementById("tasks")
+  const taskLi = document.createElement("li")
+  
+  taskLi.textContent = task
+  taskUl.appendChild(taskLi)
+}
